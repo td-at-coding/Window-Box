f8e06e17db5fd9c6f82f610e9414352061031cef
td-at-coding <td_1995@live.com>
2019-04-29 01:27:33 -0400

    new button
    changed to svg

--- src/index.js
+++ src/index.js
@@ -8,7 +8,24 @@ function App() {
     <div className="App">
       <div className="Block">
         <div className="Exit-Button">
-          <div className="text">X</div>
+          <svg width="40" height="40">
+            <line
+              x1={0}
+              y1={0}
+              x2={40}
+              y2={40}
+              stroke="black"
+              strokeWidth="2.5"
+            />
+            <line
+              x1={0}
+              y1={40}
+              x2={40}
+              y2={0}
+              stroke="black"
+              strokeWidth="2.5"
+            />
+          </svg>
         </div>
       </div>
     </div>
